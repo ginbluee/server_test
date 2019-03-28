d60a4f16bc9b4ad83a8b967608d6acc2ce546e3b
Dương Thanh Hiếu <39717177+ginbluee@users.noreply.github.com>
2019-03-28 09:32:02 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+let port = 8999 || process.env.PORT;
+
+app.get('/', (req, res) => {
+    try
+    {
+        let a = Number(req.query.a) || 0
+        let b = Number(req.query.b) || 0
+        
+        let sum = a + b
+        res.json(sum)
+    }
+    catch(e)
+    {
+        res.json('Sai định dạng số')
+    }
+})
+
+app.listen(port,function(){
+    console.log("server is listening on port: ",port)
+})
